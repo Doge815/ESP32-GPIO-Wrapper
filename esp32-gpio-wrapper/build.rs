a05679e2c7fa869b72847e71b7bf31c2b525b227
Doge815 <815.doge@gmail.com>
2024-07-01 17:42:57 +0200

    add compatibility

--- esp32-gpio-wrapper/build.rs
+++ esp32-gpio-wrapper/build.rs
@@ -3,7 +3,119 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum ADC {
+    ADC1(i32),
+    ADC2(i32),
+    NOADC,
+}
+
+macro_rules! print {
+    ($file:ident, $to_write:expr) => {
+        $file.write(format!($to_write).as_bytes()).unwrap()
+    };
+}
+
 fn main() {
+    embuild::espidf::sysenv::output();
+
+    let esp32_pins: Vec<(i32, ADC)> = vec![
+        (0, ADC::ADC2(1)),
+        (1, ADC::NOADC),
+        (2, ADC::ADC2(2)),
+        (3, ADC::NOADC),
+        (4, ADC::ADC2(0)),
+        (5, ADC::NOADC),
+        (6, ADC::NOADC),
+        (7, ADC::NOADC),
+        (8, ADC::NOADC),
+        (9, ADC::NOADC),
+        (10, ADC::NOADC),
+        (11, ADC::NOADC),
+        (12, ADC::ADC2(5)),
+        (13, ADC::ADC2(4)),
+        (14, ADC::ADC2(6)),
+        (15, ADC::ADC2(3)),
+        (16, ADC::NOADC),
+        (17, ADC::NOADC),
+        (18, ADC::NOADC),
+        (19, ADC::NOADC),
+        (21, ADC::NOADC),
+        (22, ADC::NOADC),
+        (23, ADC::NOADC),
+        (25, ADC::ADC2(8)),
+        (26, ADC::ADC2(9)),
+        (27, ADC::ADC2(7)),
+        (32, ADC::ADC1(4)),
+        (33, ADC::ADC1(5)),
+        (34, ADC::ADC1(6)),
+        (35, ADC::ADC1(7)),
+        (36, ADC::ADC1(0)),
+        (37, ADC::ADC1(1)),
+        (38, ADC::ADC1(2)),
+        (39, ADC::ADC1(3)),
+    ];
+
+    let esp32s2_pins: Vec<(i32, ADC)> = vec![
+        (0, ADC::NOADC),
+        (1, ADC::ADC1(0)),
+        (2, ADC::ADC1(1)),
+        (3, ADC::ADC1(2)),
+        (4, ADC::ADC1(3)),
+        (5, ADC::ADC1(4)),
+        (6, ADC::ADC1(5)),
+        (7, ADC::ADC1(6)),
+        (8, ADC::ADC1(7)),
+        (9, ADC::ADC1(8)),
+        (10, ADC::ADC1(9)),
+        (11, ADC::ADC2(0)),
+        (12, ADC::ADC2(1)),
+        (13, ADC::ADC2(2)),
+        (14, ADC::ADC2(3)),
+        (15, ADC::ADC2(4)),
+        (16, ADC::ADC2(5)),
+        (17, ADC::ADC2(6)),
+        (18, ADC::ADC2(7)),
+        (19, ADC::ADC2(8)),
+        (20, ADC::ADC2(9)),
+        (21, ADC::NOADC),
+        (26, ADC::NOADC),
+        (27, ADC::NOADC),
+        (28, ADC::NOADC),
+        (29, ADC::NOADC),
+        (30, ADC::NOADC),
+        (31, ADC::NOADC),
+        (32, ADC::NOADC),
+        (33, ADC::NOADC),
+        (34, ADC::NOADC),
+        (35, ADC::NOADC),
+        (36, ADC::NOADC),
+        (37, ADC::NOADC),
+        (38, ADC::NOADC),
+        (39, ADC::NOADC),
+        (40, ADC::NOADC),
+        (41, ADC::NOADC),
+        (42, ADC::NOADC),
+        (43, ADC::NOADC),
+        (44, ADC::NOADC),
+        (45, ADC::NOADC),
+        (46, ADC::NOADC),
+        (47, ADC::NOADC),
+        (48, ADC::NOADC),
+    ];
+
+    let mut pins_opt = None;
+
+    let args = embuild::espidf::sysenv::cfg_args().unwrap().args;
+
+    if args.contains(&"esp32".to_string()) {
+        pins_opt = Some(esp32_pins);
+    } else if args.contains(&"esp32s3".to_string()) || args.contains(&"esp32s3".to_string()) {
+        pins_opt = Some(esp32s2_pins);
+    }
+
+    let pins = pins_opt.expect("SoC not supported");
+
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("adc.rs");
 
@@ -14,38 +126,20 @@ fn main() {
         .open(dest_path.clone())
         .unwrap();
 
-    const ADC1_PINS: [i32; 8] = [32, 33, 34, 35, 36, 37, 38, 39];
-    const ADC2_PINS: [i32; 9] = [2, 4, 12, 13, 14, 15, 25, 26, 27];
-    const NON_EXISTENT_PINS: [i32; 6] = [20, 24, 28, 29, 30, 31];
-
-    for pin in 0..40 {
-        if NON_EXISTENT_PINS.contains(&pin) {
-            continue;
-        }
-
-        let adc = if ADC1_PINS.contains(&pin) {
-            1
-        } else if ADC2_PINS.contains(&pin) {
-            2
-        } else {
-            0
+    for (pin, adc) in pins.iter() {
+        let adc_driver = match adc {
+            ADC::ADC1(_) => "adc1_driver",
+            ADC::ADC2(_) => "adc2_driver",
+            ADC::NOADC => "None",
         };
-
-        let used_adc = match adc {
-            1 => "ADC1",
-            2 => "ADC2",
-            _ => "",
+        let adc_str = match adc {
+            ADC::ADC1(_) => "ADC1",
+            ADC::ADC2(_) => "ADC2",
+            ADC::NOADC => "None",
         };
-
-        let used_adc_driver = match adc {
-            1 => "adc1_driver",
-            2 => "adc2_driver",
-            _ => "",
-        };
-
-        file.write(
-            format!(
-                "
+        print!(
+            file,
+            "
 struct GpioPin{pin}<'a> {{
     pin: Option<Gpio{pin}>,
     adc1_driver: Arc<Mutex<Option<AdcDriver<'a, hal::adc::ADC1>>>>,
@@ -70,21 +164,15 @@ unsafe impl Send for GpioPin{pin}<'_> {{}}
 unsafe impl Sync for GpioPin{pin}<'_> {{}}
 
 #[async_trait]
-impl GpioPin for GpioPin{pin}<'_> {{
-    async fn get_adc(&mut self) -> Result<u16, GpioWrapperError> {{
-"
-            )
-            .as_bytes(),
-        )
-        .unwrap();
+impl GpioPin for GpioPin{pin}<'_> {{"
+        );
 
-        if adc != 0 {
-            file.write(
-                format!(
-                    "
+        if *adc != ADC::NOADC {
+            print!(file, "
+    async fn get_adc(&mut self) -> Result<u16, GpioWrapperError> {{
         if self.pin.is_some() {{
             let pin = self.pin.as_mut().unwrap();
-            let mut adc_driver = self.{used_adc_driver}.lock().await;
+            let mut adc_driver = self.{adc_driver}.lock().await;
             if adc_driver.is_some() {{
                 let mut adc_channel_driver: AdcChannelDriver<
                     {{ hal::adc::attenuation::DB_11 }},
@@ -100,46 +188,19 @@ impl GpioPin for GpioPin{pin}<'_> {{
             return Err(GpioWrapperError::AdcNotOwned);
         }}
         Err(GpioWrapperError::PinNotOwned)
-",
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        } else {
-            file.write(
-                format!(
-                    "
-        Err(GpioWrapperError::NotAnAdcPin)
-",
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        }
-        file.write(
-            format!(
-                "
     }}
+
     async fn get_adc_averaged( &mut self, measurement: MeasurementConfig) -> Result<f32, GpioWrapperError> {{
-",
-            )
-            .as_bytes(),
-        )
-        .unwrap();
-        if adc != 0 {
-            file.write(
-                format!(
-                    "
         if measurement.to_measure == 0 {{
             return Ok(f32::NAN);
         }}
         if self.pin.is_some() {{
             let pin = self.pin.as_mut().unwrap();
-            let mut adc_driver = self.{used_adc_driver}.lock().await;
+            let mut adc_driver = self.{adc_driver}.lock().await;
             if adc_driver.is_some() {{
                 fn helper<const N: u32>(
                     pin: &mut Gpio{pin},
-                    adc_driver: &mut tokio::sync::MutexGuard<Option<AdcDriver<{used_adc}>>>,
+                    adc_driver: &mut tokio::sync::MutexGuard<Option<AdcDriver<{adc_str}>>>,
                     measurement: MeasurementConfig,
                 ) -> Result<f32, GpioWrapperError> {{
                     let mut adc_channel_driver: AdcChannelDriver<{{ N }}, Gpio{pin}> =
@@ -178,32 +239,64 @@ impl GpioPin for GpioPin{pin}<'_> {{
             return Err(GpioWrapperError::AdcNotOwned);
         }}
         Err(GpioWrapperError::PinNotOwned)
-",
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        } else {
-            file.write(
-                format!(
-                    "
-        Err(GpioWrapperError::NotAnAdcPin)
-",
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        }
-        file.write(
-            format!(
-                "
     }}
+"
+            );
+        } else {
+            print!(file, "
+    async fn get_adc(&mut self) -> Result<u16, GpioWrapperError> {{
+        Err(GpioWrapperError::NotAnAdcPin)
+    }}
+
+    async fn get_adc_averaged( &mut self, measurement: MeasurementConfig) -> Result<f32, GpioWrapperError> {{
+        Err(GpioWrapperError::NotAnAdcPin)
+    }}
+");
+        }
+        print!(
+            file,
+            "
 }}
-",
-            )
-            .as_bytes(),
-        )
-        .unwrap();
+"
+        );
     }
+    print!(file, "
+fn pins_vec(adc1: Option<ADC1>, adc2: Option<ADC2>, pins: Pins) -> Vec<Arc<Mutex<Option<Box<dyn GpioPin>>>>> {{
+        let adc1_driver = adc1.map(|adc1| {{
+            AdcDriver::new(adc1, &hal::adc::config::Config::new().calibration(true)).unwrap()
+        }});
+        let adc2_driver = adc2.map(|adc2| {{
+            AdcDriver::new(adc2, &hal::adc::config::Config::new().calibration(true)).unwrap()
+        }});
+        let adc1_ref: Arc<Mutex<Option<AdcDriver<ADC1>>>> = Arc::new(Mutex::new(adc1_driver));
+        let adc2_ref = Arc::new(Mutex::new(adc2_driver));
+    vec![
+    ");
+    for i in 0..pins.last().unwrap().0 {
+        if pins.iter().any(|(pin, _)| *pin == i) {
+            print!(
+                file,
+                "
+        Arc::new(Mutex::new(Some(Box::new(GpioPin{i}::new(
+                pins.gpio{i},
+                adc1_ref.clone(),
+                adc2_ref.clone(),
+))))),"
+            );
+        } else {
+            print!(
+                file,
+                "
+        Arc::new(Mutex::new(None)),"
+            );
+        }
+    }
+    print!(
+        file,
+        "
+    ]
+}}
+    "
+    );
     println!("cargo::rerun-if-changed=build.rs");
 }
--- esp32-gpio-wrapper/src/lib.rs
+++ esp32-gpio-wrapper/src/lib.rs
@@ -54,7 +54,7 @@ impl From<EspError> for GpioWrapperError {
 }
 
 #[async_trait]
-pub trait GpioPin: Send + Sync {
+trait GpioPin: Send + Sync {
     async fn get_adc(&mut self) -> Result<u16, GpioWrapperError>;
     async fn get_adc_averaged(
         &mut self,
@@ -76,108 +76,6 @@ pub struct MeasurementConfig {
 }
 
 include!(concat!(env!("OUT_DIR"), "/adc.rs"));
-
-/*struct GpioPin32<'a> {
-    pin: Option<Gpio32>,
-    adc1_driver: Arc<Mutex<Option<AdcDriver<'a, hal::adc::ADC1>>>>,
-    adc2_driver: Arc<Mutex<Option<AdcDriver<'a, hal::adc::ADC2>>>>,
-}
-
-impl<'a> GpioPin32<'a> {
-    fn new(
-        pin: Gpio32,
-        adc1_driver: Arc<Mutex<Option<AdcDriver<'a, hal::adc::ADC1>>>>,
-        adc2_driver: Arc<Mutex<Option<AdcDriver<'a, hal::adc::ADC2>>>>,
-    ) -> Self {
-        GpioPin32 {
-            pin: Some(pin),
-            adc1_driver,
-            adc2_driver,
-        }
-    }
-}
-
-#[async_trait]
-impl GpioPin for GpioPin32<'_> {
-    async fn get_adc(&mut self) -> Result<u16, GpioWrapperError> {
-        if self.pin.is_some() {
-            let pin = self.pin.as_mut().unwrap();
-            let mut adc_driver: tokio::sync::MutexGuard<Option<AdcDriver<ADC1>>> =
-                self.adc1_driver.lock().await;
-            if adc_driver.is_some() {
-                let mut adc_channel_driver: AdcChannelDriver<
-                    { hal::adc::attenuation::DB_11 },
-                    Gpio32,
-                > = AdcChannelDriver::new((pin).into_ref()).unwrap();
-                let readout = adc_driver
-                    .as_mut()
-                    .unwrap()
-                    .read(&mut adc_channel_driver)
-                    .map_err(GpioWrapperError::from);
-                return readout;
-            }
-            return Err(GpioWrapperError::AdcNotOwned);
-        }
-        Err(GpioWrapperError::PinNotOwned)
-    }
-
-    async fn get_adc_averaged(
-        &mut self,
-        measurement: MeasurementConfig,
-    ) -> Result<f32, GpioWrapperError> {
-        if measurement.to_measure == 0 {
-            return Ok(f32::NAN);
-        }
-        if self.pin.is_some() {
-            let pin = self.pin.as_mut().unwrap();
-            let mut adc_driver = self.adc1_driver.lock().await;
-            if adc_driver.is_some() {
-                fn helper<const N: u32>(
-                    pin: &mut Gpio32,
-                    adc_driver: &mut tokio::sync::MutexGuard<Option<AdcDriver<ADC1>>>,
-                    measurement: MeasurementConfig,
-                ) -> Result<f32, GpioWrapperError> {
-                    let mut adc_channel_driver: AdcChannelDriver<{ N }, Gpio32> =
-                        AdcChannelDriver::new((pin).into_ref()).unwrap();
-                    let reader = adc_driver.as_mut().unwrap();
-                    let mut sum = 0.0;
-                    for _ in 0..measurement.to_measure {
-                        sum += reader
-                            .read(&mut adc_channel_driver)
-                            .map_err(GpioWrapperError::from)? as f32;
-                    }
-                    return Ok(sum as f32 / measurement.to_measure as f32);
-                }
-                const DB_0: u32 = hal::sys::adc_atten_t_ADC_ATTEN_DB_0;
-                const DB_2_5: u32 = hal::sys::adc_atten_t_ADC_ATTEN_DB_2_5;
-                const DB_6: u32 = hal::sys::adc_atten_t_ADC_ATTEN_DB_6;
-                const DB_11: u32 = hal::sys::adc_atten_t_ADC_ATTEN_DB_11;
-                const DB_12: u32 = hal::sys::adc_atten_t_ADC_ATTEN_DB_12; //not used
-
-                match measurement.attenuation {
-                    Attenuation::DB0 => {
-                        return helper::<DB_0>(pin, &mut adc_driver, measurement);
-                        //cannot use the constant directly, wtf rust???
-                    }
-                    Attenuation::DB2_5 => {
-                        return helper::<DB_2_5>(pin, &mut adc_driver, measurement);
-                    }
-                    Attenuation::DB6 => {
-                        return helper::<DB_6>(pin, &mut adc_driver, measurement);
-                    }
-                    Attenuation::DB11 => {
-                        return helper::<DB_11>(pin, &mut adc_driver, measurement);
-                    }
-                }
-            }
-            return Err(GpioWrapperError::AdcNotOwned);
-        }
-        Err(GpioWrapperError::PinNotOwned)
-    }
-}
-
-unsafe impl Send for GpioPin32<'_> {}
-unsafe impl Sync for GpioPin32<'_> {}*/
 
 pub struct PinWrapper {
     pin: Arc<Mutex<Option<Box<dyn GpioPin>>>>,
@@ -212,214 +110,18 @@ pub struct GpioWrapper {
 
 impl GpioWrapper {
     pub fn new(adc1: Option<ADC1>, adc2: Option<ADC2>, pins: Pins) -> Self {
-        let adc1_driver = adc1.map(|adc1| {
-            AdcDriver::new(adc1, &hal::adc::config::Config::new().calibration(true)).unwrap()
-        });
-        let adc2_driver = adc2.map(|adc2| {
-            AdcDriver::new(adc2, &hal::adc::config::Config::new().calibration(true)).unwrap()
-        });
-        let adc1_ref = Arc::new(Mutex::new(adc1_driver));
-        let adc2_ref = Arc::new(Mutex::new(adc2_driver));
-
-        let pins: Vec<Arc<Mutex<Option<Box<dyn GpioPin>>>>> = vec![
-            Arc::new(Mutex::new(Some(Box::new(GpioPin0::new(
-                pins.gpio0,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin1::new(
-                pins.gpio1,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin2::new(
-                pins.gpio2,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin3::new(
-                pins.gpio3,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin4::new(
-                pins.gpio4,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin5::new(
-                pins.gpio5,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin6::new(
-                pins.gpio6,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin7::new(
-                pins.gpio7,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin8::new(
-                pins.gpio8,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin9::new(
-                pins.gpio9,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin10::new(
-                pins.gpio10,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin11::new(
-                pins.gpio11,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin12::new(
-                pins.gpio12,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin13::new(
-                pins.gpio13,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin14::new(
-                pins.gpio14,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin15::new(
-                pins.gpio15,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin16::new(
-                pins.gpio16,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin17::new(
-                pins.gpio17,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin18::new(
-                pins.gpio18,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin19::new(
-                pins.gpio19,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin20::new(pins.gpio20, adc1_ref.clone(), adc2_ref.clone()))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin21::new(
-                pins.gpio21,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin22::new(
-                pins.gpio22,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin23::new(
-                pins.gpio23,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin24::new(pins.gpio24, adc1_ref.clone(), adc2_ref.clone()))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin25::new(
-                pins.gpio25,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin26::new(
-                pins.gpio26,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin27::new(
-                pins.gpio27,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin28::new(pins.gpio28, adc1_ref.clone(), adc2_ref.clone()))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin29::new(pins.gpio29, adc1_ref.clone(), adc2_ref.clone()))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin30::new(pins.gpio30, adc1_ref.clone(), adc2_ref.clone()))))),
-            //Arc::new(Mutex::new(Some(Box::new(GpioPin31::new(pins.gpio31, adc1_ref.clone(), adc2_ref.clone()))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin32::new(
-                pins.gpio32,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin33::new(
-                pins.gpio33,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin34::new(
-                pins.gpio34,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin35::new(
-                pins.gpio35,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin36::new(
-                pins.gpio36,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin37::new(
-                pins.gpio37,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin38::new(
-                pins.gpio38,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-            Arc::new(Mutex::new(Some(Box::new(GpioPin39::new(
-                pins.gpio39,
-                adc1_ref.clone(),
-                adc2_ref.clone(),
-            ))))),
-        ];
-        GpioWrapper { pins }
+        GpioWrapper {
+            pins: pins_vec(adc1, adc2, pins),
+        }
     }
 
     pub fn get_pin(&self, pin: usize) -> Result<PinWrapper, GpioWrapperError> {
-        const NON_EXISTENT_PINS: [usize; 6] = [20, 24, 28, 29, 30, 31];
-        let mut index = pin as usize;
-
-        if NON_EXISTENT_PINS.contains(&index) || index >= 40 {
+        if pin >= self.pins.len() {
             return Err(GpioWrapperError::PinDoesNotExist);
         }
 
-        for i in NON_EXISTENT_PINS.iter() {
-            if pin >= *i {
-                index -= 1;
-            } else {
-                break;
-            }
-        }
-
         Ok(PinWrapper {
-            pin: self.pins.get(index).unwrap().clone(),
+            pin: self.pins.get(pin).unwrap().clone(),
         })
     }
 }
